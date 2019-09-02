716baa398993ca060c1f484df536ff6775a3ac08
Dan Staples <dan@disman.tl>
2019-09-02 15:15:17 -0400

    Update api endpoint uri in frontend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   apiGateway: {
     REGION: 'us-east-1',
-    URL: 'https://9dub6znwzk.execute-api.us-east-1.amazonaws.com/prod'
+    URL: 'https://s8jx9xf751.execute-api.us-east-1.amazonaws.com/dev'
   }
 };
