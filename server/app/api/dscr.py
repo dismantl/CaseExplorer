5fac33d4376a43f2fc3c995eade313a21256e90f
Dan Staples <dan@disman.tl>
2020-07-15 16:13:10 -0400

    Add API endpoint to query for all district criminal court cases listing the given officer sequence number

--- server/app/api/dscr.py
+++ server/app/api/dscr.py
@@ -3,7 +3,7 @@ from flask_accepts import accepts
 from flask_restx import Namespace, Resource
 
 from app.api.interface import QueryParams
-from app.models import DSCR
+from app.models import DSCR, DSCRRelatedPerson
 from app.service import DataService
 from app.utils import get_eager_query
 
@@ -40,5 +40,16 @@ def api_factory(schemas):
         @api.marshal_with(dscr_schema_full)
         def get(self, case_number):
             return get_eager_query(DSCR).filter(DSCR.case_number == case_number).one()
+    
+    @api.route('/bpd/<string:sequence_number>')
+    class BPD(Resource):
+        '''Returns a list of case numbers involving the officer with the given Sequence Number'''
+
+        def get(self, sequence_number):
+            print(sequence_number)
+            related_persons = DSCRRelatedPerson.query.filter(DSCRRelatedPerson.officer_id == sequence_number).all()
+            case_numbers = list(dict.fromkeys([x.case_number for x in related_persons]))
+            print(case_numbers)
+            return case_numbers
 
     return api
